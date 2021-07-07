2c18781cf87573a6728f02d798e720c642afb565
Valluri Satyaprakash <valluri.satyaprakash@in.abb.com>
2021-07-07 18:36:33 +0530

    Using CSS modules for Stlying - base code with media queries written

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
+import Landingpage from "./component/Landingpage";
 function App() {
-  return <div className="App">Make a Test now.!</div>;
+  return (
+    <div className="App">
+      <Landingpage></Landingpage>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/Landingpage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import landingpagestyle from "./Landingpage.module.css";
+
+function Landingpage() {
+  return <div className={landingpagestyle.mainpage}>landing page</div>;
+}
+
+export default Landingpage;
